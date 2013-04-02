70e5f99249c2d7a774882a2cefdcbc62e9365b7b
lixiaolei19821218 <735534185@qq.com>
2013-04-02 16:50:34 +0800

    add Autosize project

---
+++ Autosize/BIDAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  BIDAppDelegate.h
+//  Autosize
+//
+//  Created by 李 潇磊 on 13-4-2.
+//  Copyright (c) 2013年 李 潇磊. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class BIDViewController;
+
+@interface BIDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) BIDViewController *viewController;
+
+@end
---
+++ Autosize/BIDViewController.h
@@ -0,0 +1,13 @@
+//
+//  BIDViewController.h
+//  Autosize
+//
+//  Created by 李 潇磊 on 13-4-2.
+//  Copyright (c) 2013年 李 潇磊. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDViewController : UIViewController
+
+@end
